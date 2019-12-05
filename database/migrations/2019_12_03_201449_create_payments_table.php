54dd00552b813429ea75716bf266bd4e3d0188c1
Rafael Gamez Diaz <gamezr@zippyttech.com>
2019-12-05 13:25:56 -0400

    Add migration to remove date field

--- database/migrations/2019_12_03_201449_create_payments_table.php
+++ database/migrations/2019_12_03_201449_create_payments_table.php
@@ -16,6 +16,7 @@ class CreatePaymentsTable extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->increments('id');
             $table->decimal('amount')->unsigned();
+            $table->date('date');
             $table->integer('journal_id')->unsigned();
             $table->integer('payment_method_id')->unsigned();
             $table->integer('client_id')->unsigned();
